cadda6e1cb20446a7589c00ae043c43c0ae19471
Minsecrus <2972853299@qq.com>
2024-05-04 22:35:24 +0800

    小修改

--- src/components/Tutorial/Chapter01.jsx
+++ src/components/Tutorial/Chapter01.jsx
@@ -1,9 +1,9 @@
 import './Chapter.css';
-import {FaClock} from "react-icons/fa6";
-import {GiPositionMarker} from "react-icons/gi";
-import {Link} from "react-router-dom";
-import {FaArrowRight} from "react-icons/fa";
-import {parseSentence} from "../mess/markdownparser";
+import { FaClock } from "react-icons/fa6";
+import { GiPositionMarker } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import { parseSentence } from "../mess/markdownparser";
 
 function Chapter01() {
     let para01 = "在这个章节中，我们将初步认识**C语言**，了解它的**历史**和**发展**。";
@@ -11,40 +11,40 @@ function Chapter01() {
     return (
         <div className="Chapter">
             <div className="Chapter-Top">
-                <h1>[1/7 章]: 概述 <span><FaClock/> 20分钟 [1/5 页]</span></h1>
-                <hr/>
+                <h1>[1/7 章]: 概述 <span><FaClock /> 20分钟 [1/5 页]</span></h1>
+                <hr />
             </div>
             <div className="Chapter-Main">
                 <div className="Chapter-Left">
                     <p onClick={() => {
                         const section2 = document.querySelector("#hello-world-01");
-                        section2.scrollIntoView({behavior: "smooth"});
+                        section2.scrollIntoView({ behavior: "smooth" });
                     }}>历史</p>
                     <p onClick={() => {
                         const section2 = document.querySelector("#hello-world-02");
-                        section2.scrollIntoView({behavior: "smooth"});
+                        section2.scrollIntoView({ behavior: "smooth" });
                     }}>语言标准</p>
                 </div>
                 <div className="Chapter-Right">
                     <div className="Current-Position">
-                        <GiPositionMarker/>
+                        <GiPositionMarker />
                         <a href='/'>主页</a>
-                        <h6>></h6>
+                        <h6>&gt;</h6>
                         <a href="/tutorial">引导式教程</a>
-                        <h6>></h6>
+                        <h6>&gt;</h6>
                         <h6>概述</h6>
                     </div>
-                    <div className="Latest-Update"><FaClock/> <h6>最近更新 2024-05-02 | Mdr</h6></div>
-                    <p dangerouslySetInnerHTML={{__html: parseSentence(para01)}}></p>
+                    <div className="Latest-Update"><FaClock /> <h6>最近更新 2024-05-02 | Mdr</h6></div>
+                    <p dangerouslySetInnerHTML={{ __html: parseSentence(para01) }}></p>
                     <h2 id="hello-world-01">历史</h2>
-                    <p dangerouslySetInnerHTML={{__html: parseSentence(para02)}}></p>
+                    <p dangerouslySetInnerHTML={{ __html: parseSentence(para02) }}></p>
                     <h2 id="hello-world-02">语言标准</h2>
                     <div className="Chapter-Bottom">
                         <div className="Chapter-Bottom-Left">
 
                         </div>
                         <div className="Chapter-Bottom-Right">
-                            <Link to="/tutorial/01/1">下一页<FaArrowRight/></Link>
+                            <Link to="/tutorial/01/1">下一页<FaArrowRight /></Link>
                         </div>
                     </div>
                 </div>
